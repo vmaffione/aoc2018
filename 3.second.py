eb6192b51be18fe3b92928b91e4fa13d479b73d8
Vincenzo Maffione <v.maffione@gmail.com>
2018-12-17 19:36:31 +0100

    3.second

---
+++ 3.second.py
@@ -0,0 +1,41 @@
+import re
+
+f = open("3.first.input", "r")
+lines = f.readlines()
+f.close()
+
+n = 1024
+# Build an nxn array (matrix) with all the elements set to 0
+matrix = [0] * (n * n)
+
+for line in lines:
+    match = re.match(r'#(\d+) @ (\d+),(\d+): (\d+)x(\d+)', line)
+    claim_id = int(match.group(1))
+    left = int(match.group(2))
+    top = int(match.group(3))
+    width = int(match.group(4))
+    height = int(match.group(5))
+
+    for i in range(left, left + width):
+        for j in range(top, top + height):
+            matrix[i * n + j] += 1
+
+for line in lines:
+    match = re.match(r'#(\d+) @ (\d+),(\d+): (\d+)x(\d+)', line)
+    claim_id = int(match.group(1))
+    left = int(match.group(2))
+    top = int(match.group(3))
+    width = int(match.group(4))
+    height = int(match.group(5))
+
+    overlap = False
+    for i in range(left, left + width):
+        for j in range(top, top + height):
+            if matrix[i * n + j] > 1:
+                overlap = True
+                break
+        if overlap:
+            break
+    if not overlap:
+        print(claim_id)
+        quit()
